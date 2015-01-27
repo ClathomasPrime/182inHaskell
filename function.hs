1e728e9ef8694b0788747baa1cb272b22e32ea46
ClathomasPrime <thoma466@purdue.edu>
2015-01-27 14:34:22 -0500

    idk function something

--- function.hs
+++ function.hs
@@ -10,3 +10,9 @@ import Prelude hiding
 (.) :: (b -> c) -> (a -> b) -> (a -> c)
 --Equivalently, (.) :: (b -> c) -> (a -> b) -> a -> c
 (f . g) a = f (g a)
+
+($) :: (a -> b) -> a -> b
+f $ a = f a
+infixr 0 $
+
+
